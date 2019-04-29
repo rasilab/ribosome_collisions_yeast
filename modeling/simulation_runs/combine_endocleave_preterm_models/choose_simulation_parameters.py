06929d4ef95be839cb0ecad6d1469ef7c7bc6f25
rasi <rasi1983@gmail.com>
2019-04-28 17:39:45 -0700

    Vary preterm and endocleave rates hand in hand

--- modeling/simulation_runs/combine_endocleave_preterm_models/choose_simulation_parameters.py
+++ modeling/simulation_runs/combine_endocleave_preterm_models/choose_simulation_parameters.py
@@ -10,6 +10,7 @@ import itertools as it
 
 # include each of these params and one of its values in each simulation,
 # they are changed from the default value
+k_elong_stall_value = 0.1
 and_params = {
     # translation params
     'k_init': list(2.0**np.arange(-8, 1, 1)),
@@ -22,7 +23,7 @@ and_params = {
     'n_stall': [6],
     'x_stall': [','.join([str(x) for x in range(400, 406)])],
     'k_elong_stall': [','.join([str(np.round(k_elong_stall * 6,3))] * 6) 
-                      for k_elong_stall in [0.1]],
+                      for k_elong_stall in [k_elong_stall_value]],
 }
 
 # create a list of all parameter combinations from above dict
@@ -31,43 +32,11 @@ and_params = [list(x) for x in it.product(*[[(p, v)
                                              for v in and_params[p]]
                                             for p in and_params])]
 
-cleave_rate_list = [0.001] 
-mrna_cleave_params = list()
-for cleave_rate in cleave_rate_list:
-    mrna_cleave_params.append({
-        # co-translational cleavage params
-        # endocleavage occurs these many codons behind a-site
-        # this is the simple cleavage model
-        'l_cleave': 5,
-        'k_cleave_no_hit': cleave_rate,
-        'k_cleave_5_hit': cleave_rate,
-        'k_cleave_3_hit': 0,
-        'k_cleave_both_hit': 0,
-        'cleave_model': 'simple',
-        'cleave_rate': cleave_rate,
-    })
-
-cleave_rate_list = [0.001] 
-for cleave_rate in cleave_rate_list:
-    mrna_cleave_params.append({
-        # co-translational cleavage params
-        # endocleavage occurs these many codons behind a-site
-        # this is the collision-stimulated cleavage model
-        'l_cleave': 5,
-        'k_cleave_no_hit': 0,
-        'k_cleave_5_hit': cleave_rate,
-        'k_cleave_3_hit': 0,
-        'k_cleave_both_hit': cleave_rate,
-        'cleave_model': 'hit5',
-        'cleave_rate': cleave_rate,
-    })
-
-# convert each param combination from dict to list of tuples
-mrna_cleave_params = [list(x.items()) for x in mrna_cleave_params]
-
 preterm_intact_rate_list = [0, 0.02, 0.1, 0.5, 1, 4] 
+cleave_rate = 0.001
 preterm_intact_params = list()
 for preterm_intact_rate in preterm_intact_rate_list:
+    scale_factor = (k_elong_stall_value + preterm_intact_rate) / k_elong_stall_value
     preterm_intact_params.append({
         # this is the CSAT model
         'k_preterm_no_hit_intact': 0,
@@ -76,6 +45,18 @@ for preterm_intact_rate in preterm_intact_rate_list:
         'k_preterm_both_hit_intact': preterm_intact_rate,
         'preterm_intact_model': 'hit5',
         'preterm_intact_rate': preterm_intact_rate,
+        # co-translational cleavage params
+        # endocleavage occurs these many codons behind a-site
+        # this is the simple cleavage model
+        'l_cleave': 5,
+        # scale cleavage rate to maintain the same cleavage efficacy despite
+        # the abortive termination relieving the stall
+        'k_cleave_no_hit': 0,
+        'k_cleave_5_hit': cleave_rate * scale_factor,
+        'k_cleave_3_hit': 0,
+        'k_cleave_both_hit': cleave_rate * scale_factor,
+        'cleave_model': 'hit5',
+        'cleave_rate': cleave_rate * scale_factor,
     })
 
 # convert each param combination from dict to list of tuples
@@ -84,7 +65,7 @@ preterm_intact_params = [list(x.items()) for x in preterm_intact_params]
 # combine the 'and' parameters and parameter combinations from above
 simcount = 0
 temp = dict()
-for params in it.product(and_params, mrna_cleave_params, preterm_intact_params):
+for params in it.product(and_params, preterm_intact_params):
     temp[simcount] = dict(it.chain.from_iterable(params))
     simcount += 1
 
